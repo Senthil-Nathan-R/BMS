8fadbb86d164ed81f7e58346261afeda6d3a818a
Senthil-Nathan-R <r.senthil18112002@gamil.com>
2025-01-20 14:04:18 +0530

    changes made in backend

--- BMS-BackEnd/src/main/java/com/example/BMS_BackEnd/Config/WebConfig.java
+++ BMS-BackEnd/src/main/java/com/example/BMS_BackEnd/Config/WebConfig.java
@@ -1,23 +1,13 @@
 package com.example.BMS_BackEnd.Config;
-import org.springframework.context.annotation.Bean;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig {
-
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("https://tiny-frangollo-907e03.netlify.app") 
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
-            }
-        };
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("https://tiny-frangollo-907e03.netlify.app/");
     }
 }
